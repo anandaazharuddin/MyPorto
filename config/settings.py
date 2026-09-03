abc788f67a8f373096ddb2e12374e6c523fa4889
Ananda Az Haruddin Salima <azharuddinananda@gmail.com>
2026-09-03 13:59:48 +0700

    change again for vercel

--- config/settings.py
+++ config/settings.py
@@ -142,10 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'portofolio' / 'static',
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'portofolio' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -168,10 +166,3 @@ STORAGES = {
         'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
 }
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'portofolio' / 'static',
-]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
